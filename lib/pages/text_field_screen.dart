52b525bb4e5941ef64b9567ef96574a03000e30d
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2022-07-23 02:01:08 +0900

    雛形作成

--- lib/pages/text_field_screen.dart
+++ lib/pages/text_field_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class TextFieldScreen extends StatelessWidget {
   const TextFieldScreen({Key? key}) : super(key: key);
@@ -10,6 +11,51 @@ class TextFieldScreen extends StatelessWidget {
         title: const Text('textField'),
         backgroundColor: Colors.cyan,
       ),
+      body: const Center(
+        child: TextFieldForm(),
+      ),
     );
+  }
+}
+
+class TextFieldForm extends StatefulWidget {
+  const TextFieldForm({Key? key}) : super(key: key);
+
+  @override
+  State<TextFieldForm> createState() => _TextFieldFormState();
+}
+
+class _TextFieldFormState extends State<TextFieldForm> {
+  String _inputText = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(40.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            _inputText,
+            style: const TextStyle(
+              color: Colors.black87,
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          TextField(
+            maxLength: 10,
+            maxLengthEnforcement: MaxLengthEnforcement.none,
+            onChanged: _handleText,
+            decoration: const InputDecoration(helperText: '表示用テスト'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _handleText(String? e) {
+    setState(() {
+      _inputText = e ?? '';
+    });
   }
 }
